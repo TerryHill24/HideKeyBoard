b7c5935d8b049b3e50b1d3b0b0a87eb3462c2316
Terry Hill <s0515718@student.rockvalleycollege.edu>
2020-09-25 14:10:07 -0500

    Initial Commit

---
+++ HideKeyBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HideKeyBoard
+//
+//  Created by RVC Terry on 9/25/20.
+//  Copyright © 2020 Terry Hill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
